d8e597c205e79a4a6c9321087f773206cf2c7d93
Garrett Breaux <garrett.breaux1@gmail.com>
2021-06-11 18:39:14 -0500

    Intial Commit

---
+++ VariableExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VariableExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string mainCharacterName = "Garrett";
+            int mainAge = 31;
+            char nickName = 'G';
+            bool lovesWorkingOut = true;
+            double liftingWeight = 225;
+            decimal bodyWeight = 160.01m;
+
+            Console.WriteLine($"My name is {mainCharacterName}. I am {mainAge} years old and my friends call me {nickName}. " +
+                $"It is {lovesWorkingOut} I love to workout. My two current goals are: weigh {bodyWeight} pounds, and bench press {liftingWeight} pounds.");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
